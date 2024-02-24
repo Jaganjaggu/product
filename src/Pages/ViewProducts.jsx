8d723639d791a642d59833bd3857df96982cdf66
Jaganjaggu <jaganvofficial@gmail.com>
2024-02-24 16:58:40 +0530

    Mongodb atlas .env file updated

--- src/Pages/ViewProducts.jsx
+++ src/Pages/ViewProducts.jsx
@@ -11,7 +11,7 @@ import { addProductAPI, deleteAProductAPI, editAProductAPI, viewAProductAPI, vie
 import { BASE_URL } from '../Services/baseURL';
 
 function ViewProducts() {
- const [editMode, setEditMode] = useState(false);
+  const [editMode, setEditMode] = useState(false);
   const [editProductId, setEditProductId] = useState(null);
 
   const [showDeleteModal, setShowDeleteModal] = useState(false);
@@ -44,7 +44,7 @@ function ViewProducts() {
     const result = await viewProductAPI()
     setViewAllProducts(result.data)
   }
-  console.log(viewAllProducts,"LM");
+  console.log(viewAllProducts, "LM");
 
   useEffect(() => {
     setInitialProducts(viewAllProducts);
@@ -105,7 +105,7 @@ function ViewProducts() {
   const handleSubmit = async () => {
     const { productname, category, description, productimg } = products
 
-    const textPattern = /^[A-Za-z\s]+$/;
+    const textPattern = /^[0-9A-Za-z\s]+$/;
 
     if (!productname.trim() || !category.trim() || !description.trim() || !productimg) {
       toast.warn('Please fill all the fields!', {
@@ -115,7 +115,7 @@ function ViewProducts() {
     }
 
     if (!textPattern.test(productname.trim()) || !textPattern.test(category.trim()) || !textPattern.test(description.trim())) {
-      toast.warn('Fields must contains character only', {
+      toast.warn('Fields must contains character or numbers only', {
         // ... toast configuration ...
       });
       return;
@@ -141,13 +141,13 @@ function ViewProducts() {
         reqBody.append("category", category);
         reqBody.append("description", description);
         reqBody.append("productimg", productimg);
-  
+
         const reqHeader = {
           "Content-Type": "multipart/form-data"
         };
-  
+
         const result = await editAProductAPI(editProductId, reqBody, reqHeader);
-  
+
         if (result.status === 200) {
           toast.success('Product Edited', {
             // ... toast configuration ...
@@ -162,13 +162,13 @@ function ViewProducts() {
         reqBody.append("category", category);
         reqBody.append("description", description);
         reqBody.append("productimg", productimg);
-  
+
         const reqHeader = {
           "Content-Type": "multipart/form-data"
         };
-  
+
         const result = await addProductAPI(reqBody, reqHeader);
-  
+
         if (result.status === 200) {
           toast.success('Product Added', {
             // ... toast configuration ...
@@ -213,7 +213,7 @@ function ViewProducts() {
       description: productToEdit.description,
       productimg: imageFile
     });
-    console.log(productToEdit,"Productto edit");
+    console.log(productToEdit, "Productto edit");
     setPreview(`${BASE_URL}/uploads/${productToEdit.productimg}`);
     setEditProductId(id);
     handleShow();
@@ -229,7 +229,7 @@ function ViewProducts() {
       console.error('Error fetching image file:', error);
       return null;
     }
-  };  
+  };
 
   return (
     <>
@@ -291,38 +291,38 @@ function ViewProducts() {
 
         )
         }
-        <div className='product-list-container'>
 
-          {filteredProducts?.map((item) => (
-            <div className='product-card'>
-              <div className='product-card-section1'>
-                <div className='product-list-image'>
-                  <img src={item ? `${BASE_URL}/uploads/${item?.productimg}` : productimage} alt="Image" />
+        {
+          filteredProducts.length > 0 ? (
+            <div className='product-list-container'>
+              {filteredProducts?.map((item) => (
+                <div className='product-card'>
+                  <div className='product-card-section1'>
+                    <div className='product-list-image'>
+                      <img src={item ? `${BASE_URL}/uploads/${item?.productimg}` : productimage} alt="Image" />
+                    </div>
+                    <div className='product-list-content' style={{ overflow: 'hidden' }}>
+                      <h2>{item.productname}</h2>
+                      <h3>{item.category}</h3>
+                      <div ><p>{item.description} </p></div>
+                    </div>
+                  </div>
+                  <div className='handleUpdate-container'>
+                    <div className='edit-product' onClick={() => handleEdit(item._id)}>
+                      <i class="fa-solid fa-pen-to-square"></i>
+                    </div>
+                    <div className='delete-product' onClick={() => handleDelete(item._id)}>
+                      <i class="fa-solid fa-trash"></i>
+                    </div>
+                  </div>
                 </div>
-                <div className='product-list-content' style={{overflow:'hidden'}}>
-                  <h2>{item.productname}</h2>
-                  <h3>{item.category}</h3>
-                  <div ><p>{item.description} </p></div>
-                </div>
-              </div>
-              <div className='handleUpdate-container'>
-                <div className='edit-product' onClick={() => handleEdit(item._id)}>
-
-                  <i class="fa-solid fa-pen-to-square"></i>
-                </div>
-                <div className='delete-product' onClick={() => handleDelete(item._id)}>
-                  <i class="fa-solid fa-trash"></i>
-                </div>
-
-              </div>
-
+              ))
+              }
 
             </div>
-          ))
-          }
+          ) : (<h5><center>No Products Found</center></h5>)
+        }
 
-
-        </div>
         <>
           <Modal
             show={show}
@@ -343,7 +343,7 @@ function ViewProducts() {
                 <div className='add-product-fields'>
                   <input value={products.productname} onChange={(e) => setProducts({ ...products, productname: e.target.value })} type="text" placeholder='Product Name' />
                   <input value={products.category} onChange={(e) => setProducts({ ...products, category: e.target.value })} type="text" placeholder='Category' />
-                  <textarea  maxLength={50}  value={products.description} onChange={(e) => setProducts({ ...products, description: e.target.value })} name="" id="" cols="30" rows="4" placeholder='Description'></textarea>
+                  <textarea maxLength={50} value={products.description} onChange={(e) => setProducts({ ...products, description: e.target.value })} name="" id="" cols="30" rows="4" placeholder='Description'></textarea>
                 </div>
               </div>
             </Modal.Body>
@@ -359,7 +359,7 @@ function ViewProducts() {
         {/* Edit Modal */}
 
 
-      </div>
+      </div >
       <ToastContainer />
     </>
   )
